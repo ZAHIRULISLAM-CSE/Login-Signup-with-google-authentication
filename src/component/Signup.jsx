c5be22ccebdc1c5407a4679924ea2e6a7a66a0bb
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-04-15 00:06:27 +0600

    completed forget password option

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -1,17 +1,45 @@
+import {
+  getAuth,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import React from "react";
+import app from "../config/config";
+import { useRef } from "react";
 
 const Signup = () => {
+  const auth = getAuth(app);
+  const emailRef = useRef();
 
-    const handleFormDetails=(event)=>{
-        // setSuccessMessage('');
-        // setErrorMessage('');
-        event.preventDefault();
-        const password=event.target.password.value;
-        const email=event.target.email.value;
-        console.log(password);
-        console.log(email);
-
-    }
+  const handleFormDetails = (event) => {
+    // setSuccessMessage('');
+    // setErrorMessage('');
+    event.preventDefault();
+    const password = event.target.password.value;
+    const email = event.target.email.value;
+    console.log(password);
+    console.log(email);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+  const handleForgetPassWord = () => {
+    const email = emailRef.current.value;
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        alert('check your email');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage)
+        // ..
+      });
+  };
   return (
     <div>
       <form onSubmit={handleFormDetails} className="w-1/2 mx-auto">
@@ -23,6 +51,7 @@ const Signup = () => {
             Your email
           </label>
           <input
+            ref={emailRef}
             type="email"
             id="email"
             name="email"
@@ -67,6 +96,13 @@ const Signup = () => {
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Submit
+        </button>
+        <button
+          onClick={handleForgetPassWord}
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Forget Password
         </button>
         {/* <p className="text-2xl mt-4 text-green-600">{successMessage}</p>
         <p className="text-2xl mt-4 text-red-600">{error}</p> */}
